08ebe64e0d3859c02ebfa9e7f50cebfed398417f
P Ganesh <nagpichikaganesh@gmail.com>
2024-09-25 13:05:08 +0530

    Index of key in arr

---
+++ Day__11__Cpp/ques2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int size;
+    cout << "size: ";
+    cin >> size;
+    vector<int> arr(size);
+    for(int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+    int key;
+    cout << "key: ";
+    cin >> key;
+    bool flag = false;
+    for(int i = 0; i < size; i++){
+        if(arr[i] == key){
+            cout << "pos: " << i;
+            flag = true;
+            break;
+        }
+    }
+    if(flag == false){
+    cout << "not found";
+    }
+}
